c986dc92ee5a4b310668e3b772d32bcb62ef0611
choosunsick <random@example.com>
2019-05-18 20:43:22 +0900

    return 값 파싱함수 쓸 수 있게 변경

--- api/__init__.py
+++ api/__init__.py
@@ -2,9 +2,15 @@ from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
 def get_game(date, home_team, away_team, double=0):
-    url = (f'https://www.koreabaseball.com/Schedule/GameCenter/Main.aspx?gameDate={date}&gameId={date}{home_team}{away_team}{double}&section=REVIEW')
+    url = f'https://www.koreabaseball.com/Schedule/GameCenter/Main.aspx?gameDate={date}&gameId={date}{home_team}{away_team}{double}&section=REVIEW'
+    gameld = f'{date}{home_team}{away_team}{double}'
     session = HTMLSession()
     r = session.get(url)
     r.html.render()
     soup = BeautifulSoup(r.html.html, "lxml")
-    return soup
+    tables = soup.find_all('table')
+    record_etc = soup.findAll('div',{'class':'record-etc'})
+    box_score = soup.findAll('div',{'class':'box-score-wrap'})
+    teams = box_score[0].findAll('span',{'class':'logo'})
+    return{'tables':tables, 'record_etc':record_etc, 'teams':teams, 'date':date, 'id':gameld}
+    
