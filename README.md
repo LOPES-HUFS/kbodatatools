406ef37735a260ffa4479b2a978210499841a4ab
Jaehwan <dialektike@gmail.com>
2019-05-18 19:30:38 +0900

    개별 game 자료 받기

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # KBO_data_analysis
+
 KBO 자료를 모으고 분석하고자 합니다.
+
+## 사용법
+
+게임을 받아오는 방법
+
+```python
+import api
+api.get_game(date=20190511, home_team= 'OB', away_team='NC')
+```
---
+++ api/__init__.py
@@ -0,0 +1,11 @@
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+def get_game(date, home_team, away_team, double=0):
+    url = (f'https://www.koreabaseball.com/Schedule/GameCenter/Main.aspx?gameDate={date}&gameId={date}{home_team}{away_team}{double}&section=REVIEW')
+    session = HTMLSession()
+    r = session.get(url)
+    r.html.render()
+    r.html.text
+    soup = BeautifulSoup(r.html.full_text, "lxml")
+    print(soup)
