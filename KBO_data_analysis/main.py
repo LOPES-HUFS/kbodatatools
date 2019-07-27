6a2696648e03f7a120009e7728ad18860e60e2b4
choosunsick <random@example.com>
2019-07-27 16:47:32 +0900

    api 파일들 구분

--- KBO_data_analysis/main.py
+++ KBO_data_analysis/main.py
@@ -2,7 +2,7 @@
 gameid를 통해 전체 데이터 크롤링
 '''
 
-import .init from get_game, get_data, modify_data
+import api 
 import pandas as pd 
 import json
 
@@ -15,11 +15,11 @@ def stack_game_data(gameid):
     '''
     full_data = {}
 
-    for i in range(0,len(gameid)):
+    for i in range(0,12):
         index = str(gameid.date[i])+gameid.gameid[i]
         print(index)
-        temp_data = get_data(get_game(date=gameid.date[i], home_team= gameid.home[i], away_team=gameid.away[i]))
-        temp_data = modify_data(temp_data)
+        temp_data = api.get_data(api.get_game(date=gameid.date[i], home_team= gameid.home[i], away_team=gameid.away[i]))
+        temp_data = api.modify_data(temp_data)
         full_data.update({index:temp_data})
 
     return full_data
