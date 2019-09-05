55ef71510c5cead1944911bc6b9d99461ce78b31
choosunsick <random@example.com>
2019-09-05 18:41:58 +0900

    패치용 파일 추가

--- KBO_data_analysis/patch.py
+++ KBO_data_analysis/patch.py
@@ -16,8 +16,8 @@ for i in batter_data.index[(batter_data["id"]==0) & (batter_data["선수명"] ==
     batter_data.date.loc[i] = str(batter_data.year.loc[i])+"-"+ batter_data.dateindex.loc[i][4:6]+"-"+batter_data.dateindex.loc[i][6:8]
 
 
-data_97109 = pd.read_csv("./data/data_97109.csv")
-data_76100 = pd.read_csv("./data/data_76100.csv")
+data_97109 = pd.read_csv("./data/patch_file/data_97109.csv")
+data_76100 = pd.read_csv("./data/patch_file/data_76100.csv")
 
 date_76100=[i for i in list(batter_data.date[(batter_data["선수명"] == "이병규")& (batter_data.id==0)]) if i not in list(data_97109.date)]
 date_97109=[i for i in list(batter_data.date[(batter_data["선수명"] == "이병규")& (batter_data.id==0)]) if i not in list(data_76100.date)]
@@ -37,7 +37,7 @@ for i in duplicate_date:
 
 data_76100_duplicated.columns = ['date','away','home','doubleheader','name','team',"1","2","3","4","5","6","7","8","9",'ten','eleven','twelve','ab','h','rbi','r','id']
 
-def compare_record(gamedate):
+def compare_lee_record(gamedate):
     record_index=batter_data[(batter_data["선수명"] == "이병규")&(batter_data.date==gamedate)].index
     record_76100=data_76100_duplicated[data_76100_duplicated.date==gamedate][["1","2","3","4","5","6","7","8","9"]].values.tolist()[0] 
     for i in record_index:
@@ -53,3 +53,4 @@ for i in data_76100_duplicated.date:
     batter_data=compare_record(i)
 
 # 이승호 허준혁 도 마찬가지로 진행 ㄱ 
+
