df7e5876dae0968ae35c245e51f24c0a5d46af33
choosunsick <random@example.com>
2019-09-21 21:02:44 +0900

    파일 위치 수정

--- KBO_data_analysis/api/bring_datas.py
+++ KBO_data_analysis/api/bring_datas.py
@@ -4,9 +4,9 @@
 
 import pandas as pd
 
-batter_data = pd.read_csv("./data/sample/KBO_batter_data_full.csv")
-pitcher_data = pd.read_csv("./data/sample/KBO_pitcher_data_full.csv")
-player_data = pd.read_csv("./data/KBO_player_info_full.csv")
+batter_data = pd.read_csv("../data/sample/KBO_batter_data_full.csv")
+pitcher_data = pd.read_csv("../data/sample/KBO_pitcher_data_full.csv")
+player_data = pd.read_csv("../data/KBO_player_info_full.csv")
 
 def make_month_column(data):
     '''
