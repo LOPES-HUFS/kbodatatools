0a29cd89140323db8291be7f96a0054a3014f213
choosunsick <random@example.com>
2019-09-24 18:00:16 +0900

    주석 수정

--- KBO_data_analysis/save_all_games.py
+++ KBO_data_analysis/save_all_games.py
@@ -1,5 +1,5 @@
 '''
-전체 경기 데이터 저장하는 코드 파일 샘플용 파일이 아닌 2010년부터 2019년 7월 18일까지의 
+전체 경기 데이터 저장하는 코드 파일 샘플용 파일이 아닌 2010년부터 2019년 8월까지의 
 경기 데이터를 타자와 투수 데이터로 나누어서 저장하는 코드 
 '''
 
@@ -15,6 +15,8 @@ main.write_json(gamedict)
 
 with open("./data/sample/all_data.json", 'r') as outfile:
     playerdata = json.load(outfile)
+
+# 타자 기록을 CSV파일로 저장
 
 temp = pd.DataFrame()
 
@@ -32,7 +34,7 @@ temp['선수명'][temp['선수명']=="스몰린스"] = "스몰린스키"
 temp['선수명'][temp['선수명']=="반슬라이"] = "반슬라이크"
 temp.to_csv("./data/sample/KBO_batter_data_full.csv",index=False)
 
-# 투수기록 
+# 투수 기록을 CSV파일로 저장
 
 pitcher_temp = pd.DataFrame()
 
@@ -43,5 +45,5 @@ for i in range(0,len(playerdata['fulldata'].keys())):
     away['dateindex'] = list(playerdata['fulldata'].keys())[i]
     pitcher_temp = pitcher_temp.append(away.append(home))
 
-pitcher_temp = pitcher_temp.fillna(-1)
+pitcher_temp = pitcher_temp.fillna(0)
 pitcher_temp.to_csv("./data/sample/KBO_pitcher_data_full.csv",index=False)
