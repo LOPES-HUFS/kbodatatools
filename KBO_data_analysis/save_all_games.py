2262f3b8175ceadfe33a7227612d8c9a79aa9f8c
choosunsick <random@example.com>
2019-10-08 17:46:10 +0900

    함수화

--- KBO_data_analysis/save_all_games.py
+++ KBO_data_analysis/save_all_games.py
@@ -1,5 +1,5 @@
 '''
-전체 경기 데이터 저장하는 코드 파일 샘플용 파일이 아닌 2010년부터 2019년 8월까지의 
+전체 경기 데이터 저장하는 코드 파일 샘플용 파일이 아닌 2010년부터 2019년 9월까지의 
 경기 데이터를 타자와 투수 데이터로 나누어서 저장하는 코드 
 '''
 
@@ -18,32 +18,33 @@ with open("./data/sample/all_data.json", 'r') as outfile:
 
 # 타자 기록을 CSV파일로 저장
 
-temp = pd.DataFrame()
 
-for i in range(0,len(playerdata['fulldata'].keys())):
-    away = pd.DataFrame(playerdata['fulldata'][list(playerdata['fulldata'].keys())[i]]['away_batter'])
-    home = pd.DataFrame(playerdata['fulldata'][list(playerdata['fulldata'].keys())[i]]['home_batter'])
-    home['dateindex'] = list(playerdata['fulldata'].keys())[i]
-    away['dateindex'] = list(playerdata['fulldata'].keys())[i]
-    temp = temp.append(away.append(home))
+def data2dataframe(data:json,datatype:str):
+    '''
+    경기 자료가 담긴 json 데이터를 
+    '''
+    temp = pd.DataFrame()
 
-temp = temp.fillna(0)
-temp['선수명'][temp['선수명']=="페르난데"] = "페르난데스"
-temp['선수명'][temp['선수명']=="해즐베이"] = "해즐베이커"
-temp['선수명'][temp['선수명']=="스몰린스"] = "스몰린스키"
-temp['선수명'][temp['선수명']=="반슬라이"] = "반슬라이크"
-temp.to_csv("./data/sample/KBO_batter_data_full.csv",index=False)
+    for i in range(0,len(playerdata['fulldata'].keys())):
+        away = pd.DataFrame(playerdata['fulldata'][list(playerdata['fulldata'].keys())[i]][f'away_{datatype}'])
+        home = pd.DataFrame(playerdata['fulldata'][list(playerdata['fulldata'].keys())[i]][f'home_{datatype}'])
+        home['dateindex'] = list(playerdata['fulldata'].keys())[i]
+        away['dateindex'] = list(playerdata['fulldata'].keys())[i]
+        temp = temp.append(away.append(home))
+
+    temp = temp.fillna(0)
+    return temp
+
+batter_temp = data2dataframe(playerdata,batter)
+
+batter_temp['선수명'][batter_temp['선수명']=="페르난데"] = "페르난데스"
+batter_temp['선수명'][batter_temp['선수명']=="해즐베이"] = "해즐베이커"
+batter_temp['선수명'][batter_temp['선수명']=="스몰린스"] = "스몰린스키"
+batter_temp['선수명'][batter_temp['선수명']=="반슬라이"] = "반슬라이크"
+batter_temp.to_csv("./data/sample/KBO_batter_data_full.csv",index=False)
 
 # 투수 기록을 CSV파일로 저장
 
-pitcher_temp = pd.DataFrame()
+pitcher_temp = data2dataframe(playerdata,pitcher)
 
-for i in range(0,len(playerdata['fulldata'].keys())):
-    away = pd.DataFrame(playerdata['fulldata'][list(playerdata['fulldata'].keys())[i]]['away_pitcher'])
-    home = pd.DataFrame(playerdata['fulldata'][list(playerdata['fulldata'].keys())[i]]['home_pitcher'])
-    home['dateindex'] = list(playerdata['fulldata'].keys())[i]
-    away['dateindex'] = list(playerdata['fulldata'].keys())[i]
-    pitcher_temp = pitcher_temp.append(away.append(home))
-
-pitcher_temp = pitcher_temp.fillna(0)
 pitcher_temp.to_csv("./data/sample/KBO_pitcher_data_full.csv",index=False)
