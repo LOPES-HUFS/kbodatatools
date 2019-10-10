77c279ceb4e977bb31a39eb6fa0eb821db34736d
choosunsick <random@example.com>
2019-10-10 21:25:42 +0900

    함수 설명 추가 및 init 수정

--- KBO_data_analysis/__init__.py
+++ KBO_data_analysis/__init__.py
@@ -1,4 +1,5 @@
 from .main import *
 from .basic_analysis import get_record_data
 from .make_game_info import make_game_info
+from .save_all_games import data2dataframe
 import api
--- KBO_data_analysis/save_all_games.py
+++ KBO_data_analysis/save_all_games.py
@@ -18,10 +18,15 @@ with open("./data/sample/all_data.json", 'r') as outfile:
 
 # 타자 기록을 CSV파일로 저장
 
-
 def data2dataframe(data:json,datatype:str):
     '''
-    경기 자료가 담긴 json 데이터를 
+    경기 자료가 담긴 json 데이터를 가지고 pandas Dataframe으로 만드는 함수 
+    Args:
+        data(dict): 선수들의 경기 자료가 담긴 딕트 또는 json 파일  
+        datatype(str): 타자 기록인지 투수 기록인지를 확인하는 인자로 
+        타자면 batter 투수면 pitcher를 입력하여 각각 기록을 데이터프레임에 저장할 수 있다
+    Returns:
+        temp(Pandas DF): 타자기록 또는 투수기록이 일자별로 쌓인 데이터 프레임 
     '''
     temp = pd.DataFrame()
 
